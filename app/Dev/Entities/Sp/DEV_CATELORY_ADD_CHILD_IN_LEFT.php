52cb495dc4a20f6638a5588f339b3e2f35c46dab
thanhnv-acv <thanhnv@atmarkcafe.org>
2018-07-18 09:11:24 +0700

    Build backend

---
+++ app/Dev/Entities/Sp/DEV_CATELORY_ADD_CHILD_IN_LEFT.php
@@ -0,0 +1,11 @@
+<?php 
+//This is dev automatic generate 
+ namespace App\Dev\Entities; 
+use App\Core\Entities\Entity; 
+class DEV_CATELORY_ADD_CHILD_IN_LEFT extends Entity{
+	public $code;
+	public $data_error;
+	public  function __construct($object){
+		 parent::__construct($object);
+	}
+} 
---
+++ app/Dev/Entities/Sp/DEV_CATELORY_ADD_SIBLING.php
@@ -0,0 +1,11 @@
+<?php 
+//This is dev automatic generate 
+ namespace App\Dev\Entities; 
+use App\Core\Entities\Entity; 
+class DEV_CATELORY_ADD_SIBLING extends Entity{
+	public $code;
+	public $data_error;
+	public  function __construct($object){
+		 parent::__construct($object);
+	}
+} 
