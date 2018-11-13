7221e79c56a2f06c452e6336f561232c6015b427
SonMT <sonmt@atmarkcafe.org>
2018-11-13 18:03:14 +0700

    13/11 vuejs  base laravel sample

---
+++ resources/assets/js/components/UserManager.vue
@@ -0,0 +1,28 @@
+<template id="hello-template">
+    <h1>{{ message }} {{ userName }}</h1>
+</template>
+
+<script>
+    // export default {
+    //     name: "UserManager"
+    // }
+    Vue.component('hello-component',{
+        data: function () {
+            return {
+                message: 'Xin chào'
+            }
+        },
+        props: ['userName'],
+        template: '#hello-template'
+    })
+    new Vue({
+        el: '#app',
+        data: {
+            userName: 'Allaravel'
+        }
+    });
+</script>
+
+<style scoped>
+
+</style>
