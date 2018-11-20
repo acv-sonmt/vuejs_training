d0b75948a0a27deda9b9b5b97f07bd59a58580e8
SonMT <sonmt@atmarkcafe.org>
2018-11-20 18:20:24 +0700

    tìm hiểu vue route

---
+++ app/Frontend/Http/Controllers/RoleController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SonMT
+ * Date: 11/20/2018
+ * Time: 9:33 AM
+ */
+
+namespace App\Frontend\Http\Controllers;
+
+
+class RoleController
+{
+
+}
---
+++ app/Frontend/Models/Roles.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SonMT
+ * Date: 11/20/2018
+ * Time: 9:32 AM
+ */
+
+namespace App\Frontend\Models;
+
+
+class Roles
+{
+
+}
